342d3ff40ec4b59af5fd07f360ad76097131549b
HannanLK <cb011253@students.apiit.lk>
2025-02-01 20:13:34 +0530

    Footer Update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="py-10 mt-auto bg-black">
-    <div class="container grid gap-8 px-4 mx-auto sm:px-6 lg:px-8 md:grid-cols-2 lg:grid-cols-4">
+    <div class="container grid gap-6 px-4 mx-auto sm:px-6 lg:px-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-8">
         <!-- Left Section: Store Info and Logo -->
-        <div class="flex flex-col text-gray-400">
+        <div class="flex flex-col text-gray-400 md:text-left">
             <a href="/">
-                <img src="{{ asset(path: 'assets\image\banner\logo3.png') }}" alt="Glitz Logo" class="w-40 mb-1">
+                <img src="{{ asset('assets/image/banner/logo3.png') }}" alt="Glitz Logo" class="w-40 mb-3">
             </a>
             <p class="mb-4 tracking-widest italic text-white">Where style meets elegance.</p>
             <p>388, Union Place, Colombo 02<br> 0768535555</p>
@@ -11,7 +11,7 @@
         </div>
 
         <!-- Quick Links -->
-        <div>
+        <div class="md:text-left">
             <h3 class="mb-4 text-lg text-white">Quick Links</h3>
             <ul class="space-y-2">
                 <li><a href="{{ route('shop.men') }}" class="text-sm text-gray-400 hover:text-white">Mens Clothing</a></li>
@@ -22,29 +22,29 @@
         </div>
 
         <!-- Newsletter -->
-        <div>
+        <div class="md:text-left">
             <h3 class="mb-4 text-lg text-white">NEWSLETTER</h3>
             <p class="mb-4 text-sm text-gray-400">Signup to our Newsletter For Latest Trends!</p>
             <form action="#" method="POST" class="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-2">
-                <input type="email" name="email" placeholder="Your Email" class="flex-1 px-4 py-2 text-sm text-white bg-transparent border rounded-md focus:outline-none">
+                <input type="email" name="email" placeholder="Your Email" class="flex-1 px-4 py-2 text-sm text-white bg-transparent border border-gray-400 rounded-md focus:outline-none">
                 <button type="submit" class="px-4 py-2 bg-gray-700 rounded-md hover:bg-gray-600">
-                    <i class="fa-regular fa-envelope fa-lg" style="color: #ffffff;"></i>
+                    <i class="fa-regular fa-envelope fa-lg text-white"></i>
                 </button>
             </form>
             <div class="flex mt-4 space-x-4">
                 <a href="https://x.com/HannanMunas">
-                    <img src="{{ asset(path: 'assets\image\banner\x.png') }}" alt="Twitter" class="h-6">
+                    <img src="{{ asset('assets/image/banner/x.png') }}" alt="Twitter" class="h-6">
                 </a>
                 <a href="https://www.instagram.com/hannan.lk/">
-                    <img src="{{ asset('assets\image\banner\instagram.png') }}" alt="Instgram" class="h-6">
+                    <img src="{{ asset('assets/image/banner/instagram.png') }}" alt="Instagram" class="h-6">
                 </a>
                 <a href="https://www.linkedin.com/in/hannanlk/">
-                    <img src="{{ asset('assets\image\banner\linkedin.png') }}" alt="linkedin" class="h-6">
+                    <img src="{{ asset('assets/image/banner/linkedin.png') }}" alt="LinkedIn" class="h-6">
                 </a>
             </div>
         </div>
     </div>
-    
+
     <script src="{{ asset('assets/js/canvas.js') }}"></script>
     <script src="{{ asset('assets/js/Product.js') }}"></script>
 
@@ -53,5 +53,4 @@
     <script>
         AOS.init();
     </script>
-
 </footer>
