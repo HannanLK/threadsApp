4fbfaaa26423cb085d21da0249d0c754036bd680
HannanLK <cb011253@students.apiit.lk>
2025-02-03 14:52:29 +0530

    Final Touches

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Sierra Fashion'),
+    'name' => env('APP_NAME', 'Urban Threads'),
 
     /*
     |--------------------------------------------------------------------------
