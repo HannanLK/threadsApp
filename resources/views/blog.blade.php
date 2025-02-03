3880e44da90d6ca1417ee2e79135329cde19b6f8
HannanLK <cb011253@students.apiit.lk>
2025-02-03 12:04:23 +0530

    blog page update

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,5 +1,8 @@
 <x-app-layout>
-    <div class="h-screen bg-cover bg-center" style="background-image: url('{{ asset('assets/image/banner/vague.jpg') }}');">
-        <!-- Content Here -->
+    <div class="flex items-center justify-center min-h-screen bg-gray-900 text-white text-center p-6">
+        <div>
+            <h1 class="text-4xl font-bold">Coming Soon</h1>
+            <p class="mt-2 text-lg">We're working hard to bring you something amazing. Stay tuned!</p>
+        </div>
     </div>
 </x-app-layout>
